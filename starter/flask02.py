354e16b67f4d0235c7df9fc2040d96f1bd6fc05a
mjeastm2 <mjeastm2@gmail.com>
2021-01-26 21:35:03 -0600

    adding code

--- starter/flask02.py
+++ starter/flask02.py
@@ -1,8 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
 @app.route('/about')
 def about():
 
     about_text = """
-    Mikayla's Git Project.
+    Mikayla's Git Project
     """
     return about_text
-    
+
+
+
+if __name__=='__main__':
+
+    app.run(debug=True)
